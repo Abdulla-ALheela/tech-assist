cdf1ece5e5ca3677c242bf8ad175653fb32be684
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-01-08 19:50:18 +0300

    added a get / route ,a nvabar and created requests/index.ejs

--- controllers/requests.js
+++ controllers/requests.js
@@ -4,8 +4,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-    res.send('Hello applications index route!');
+router.get('/', async (req, res) => {
+    try {
+      res.render('requests/index.ejs');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
   });
 
 module.exports = router;
