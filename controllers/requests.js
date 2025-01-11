221a45f7cee4c65e2e1d216b25c0c062a945487f
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-01-11 18:54:11 +0300

    added router to receive PUT requests

--- controllers/requests.js
+++ controllers/requests.js
@@ -111,4 +111,30 @@ try{
 
     }
   });
+
+  router.put('/:requestId', async (req, res) => {
+    try {
+   
+      const currentUser = await User.findById(req.session.user._id);
+
+      const request = currentUser.requests.id(req.params.requestId);
+    
+      request.set(req.body);
+    
+      await currentUser.save();
+
+      res.redirect(
+        `/users/${currentUser._id}/requests/${req.params.requestId}`
+      );
+
+    } catch (error) {
+
+      console.log(error);
+      res.redirect('/');
+      
+    }
+  });
+
+
+
 module.exports = router;
