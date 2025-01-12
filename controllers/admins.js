92848e61dcc6222839d0e6395d712dedfa93efc6
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-01-12 19:38:19 +0300

    added feature to allow admins to view requests assigned to them

--- controllers/admins.js
+++ controllers/admins.js
@@ -2,13 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
-const Counter = require('../models/counter.js');
+const { default: mongoose } = require('mongoose');
+
+
 
 router.get('/', async (req, res) => {
     try {
+
+let allRequests = [];
+let i = 0;
       const currentUser = await User.findById(req.session.user._id);
+
+      const users = await User.aggregate([
+        { $unwind: "$requests" },
+        { $match: { "requests.admin":new mongoose.Types.ObjectId(req.session.user._id) } },
+      ]);
+
+
       res.render('admins/index.ejs',{
         currentUser: currentUser,
+        users: users,
       });
     } catch (error) {
   
@@ -63,5 +76,27 @@ const allUsers = await User.find();
   
     res.redirect(`/users/:userId/admins/master`);
   });
+
+
+  router.get('/:userId/:requestId', async (req, res) => {
+    try {
+    
+      const currentUser = await User.findById(req.params.userId)
+
+      const request = currentUser.requests.id(req.params.requestId);
+
+      const admin = await User.findById(request.admin)
+
+      res.render('requests/show.ejs', {
+        request: request,
+        admin:admin,
+      });
+    } catch (error) {
+   
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
 
 module.exports = router;
