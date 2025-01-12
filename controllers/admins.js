1304795d47f75852336e58d2fb2f3933db698e10
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-01-12 16:01:56 +0300

    Added the ability for master admin to make other users admin

--- controllers/admins.js
+++ controllers/admins.js
@@ -6,8 +6,7 @@ const Counter = require('../models/counter.js');
 
 router.get('/', async (req, res) => {
     try {
-    const currentUser = await User.findById(req.session.user._id);
-
+      const currentUser = await User.findById(req.session.user._id);
       res.render('admins/index.ejs',{
         currentUser: currentUser,
       });
@@ -16,6 +15,53 @@ router.get('/', async (req, res) => {
       console.log(error);
       res.redirect('/');
     }
+  });
+
+  router.get('/master', async (req, res) => {
+    try {
+    const currentUser = await User.findById(req.session.user._id);
+const allUsers = await User.find();
+      res.render('admins/master.ejs',{
+        currentUser: currentUser,
+        allUsers: allUsers,
+      });
+    } catch (error) {
+  
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.get('/:userId/edit-user', async (req, res) => {
+    try {
+
+      const currentUser = await User.findById(req.session.user._id);
+    const user = await User.findById(req.params.userId);
+
+
+      res.render('admins/edit-user.ejs',{
+        user: user,
+        currentUser: currentUser,
+      });
+    } catch (error) {
+  
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+
+  router.put("/:userId", async (req, res) => {
+ 
+
+    if (req.body.isAdmin === "on") {
+      req.body.isAdmin= true;
+    } else {
+      req.body.isAdmin = false;
+    }
+    
+    await User.findByIdAndUpdate(req.params.userId, req.body);
+  
+    res.redirect(`/users/:userId/admins/master`);
   });
 
 module.exports = router;
